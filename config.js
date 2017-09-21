28d03052ab937f979ceca6fd1369dea18dd78919
hello <jackfan@mooyoo.com.cn>
2017-09-21 16:17:47 +0800

    修改

--- config.js
+++ config.js
@@ -2,9 +2,8 @@ docute.init({
     landing: 'landing.html',
     debug: true,
     // home: 'https://raw.githubusercontent.com/egoist/docute/master/README.md',
-    repo: 'egoist/docute',
-    twitter: 'rem_rin_rin',
-    'edit-link': 'https://github.com/egoist/docute/blob/master/docs/',
+    repo: 'jackfxq/blog',
+    // twitter: 'rem_rin_rin',
     tocVisibleDepth: 3,
     nav: {
         default: [{
@@ -39,14 +38,14 @@ docute.init({
             }]
         }]
     },
-    icons: [
-        {
-            label: '关注我的微博',
-            svgId: 'i-weibo',
-            svgClass: 'weibo-icon',
-            link: 'http://weibo.com/zengxinyu'
-        }
-    ],
+    // icons: [
+    //     {
+    //         label: '关注我的微博',
+    //         svgId: 'i-weibo',
+    //         svgClass: 'weibo-icon',
+    //         link: 'http://weibo.com/zengxinyu'
+    //     }
+    // ],
     plugins: [
         docsearch({
             apiKey: '65360cf9a91d87cd455d2b286d0d89ee',
